fc21550c60dc9b87eb33fc21f0281f14b491f194
kisanbura <kisanbura48@gmail.com>
2025-11-23 22:19:53 +0000

    Update Booking.java

--- SpareBnB/Booking.java
+++ SpareBnB/Booking.java
@@ -1,2 +1,22 @@
 public class Booking {
+    private User user;
+    private Accommodation accommodation;
+    private int nights;
+    private double totalCost;
+
+    public Booking(User user, Accommodation accommodation, int nights) {
+        this.user = user;
+        this.accommodation = accommodation;
+        this.nights = nights;
+        this.totalCost = accommodation.getTotalPrice(nights);
+    }
+
+    public void printBookingDetails() {
+        System.out.println("======== BOOKING CONFIRMATION ========");
+        user.printUser();
+        accommodation.printDetails();
+        System.out.println("Nights: " + nights);
+        System.out.println("Total Cost: £" + totalCost);
+        System.out.println("======================================");
+    }
 }
