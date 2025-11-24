7ca08c8e6531a85fc9f2b1c48d337c7b78d57ed3
kisanbura <kisanbura48@gmail.com>
2025-11-24 06:25:31 +0000

    Update Main.java

--- SpareBnB/Main.java
+++ SpareBnB/Main.java
@@ -1,10 +1,10 @@
-import java.util.ArrayList;
+
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) { //main method
 
-        // Facilities
+        // Facilities creating facilities objects
         List<Facility> hotelFacilities = List.of(
                 new Facility("Pool", "Outdoor heated swimming pool"),
                 new Facility("Gym", "Fully equipped fitness centre")
@@ -25,7 +25,7 @@ public class Main {
                 new Facility("EV Charging", "Electric vehicle charging point")
         );
 
-        // Accommodations
+        // Accommodations creating accomm objects
         Accommodation hotel = new Hotel("Seaside Grand Resort", "Bournemouth", 175.50, 3, hotelFacilities, 5);
         Accommodation bnb = new BnB("Maple Cottage BnB", "Bath", 95.70, 2, bnbFacilities, 5); // roomCount = 5
         Accommodation hostel = new Hostel("The Wanderer's Lodge", "Liverpool", 45.00, 12, hostelFacilities, 6);
@@ -36,20 +36,20 @@ public class Main {
         User user2 = new User("Danny Michael", 787654321, "15/07/2000", "danny@yahoo.com");
         User user3 = new User("Andrea Smith", 755888111, "04/11/2002", "andrea@outlook.com");
 
-        // Bookings
+        // Bookings tries to book hotel for certain amount of nights
         Booking b1 = new Booking(user1, hotel, 4);
         Booking b2 = new Booking(user2, hotel, 2);
         Booking b3 = new Booking(user3, bnb, 6);
         Booking b4 = new Booking(user2, motel, 1);
 
-        b1.printBookingDetails();
+        b1.printBookingDetails();  // print all booking results
         b2.printBookingDetails();
         b3.printBookingDetails();
         b4.printBookingDetails();
 
         // Release and rebook
         hotel.releaseBooking();
-        Booking b5 = new Booking(user2, hotel, 3);
+        Booking b5 = new Booking(user2, hotel, 3); // makes it available again
         b5.printBookingDetails();
     }
 }
