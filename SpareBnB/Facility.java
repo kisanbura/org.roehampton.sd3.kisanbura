5314887a95371b990041d8f7c6b2b18ba48febb3
kisanbura <kisanbura48@gmail.com>
2025-11-24 06:24:37 +0000

    Update Facility.java

--- SpareBnB/Facility.java
+++ SpareBnB/Facility.java
@@ -1,17 +1,17 @@
 public class Facility {
-    private String name;
-    private String description;
+    private String name; //stores the name of the facility
+    private String description; //stores a short explanation of the facility
 
-    public Facility(String name, String description) {
-        this.name = name;
+    public Facility(String name, String description) { //constructor runs when a facility object is created.
+        this.name = name; //saves the given name of the object
         this.description = description;
     }
 
     public String getName() {
-        return name;
+        return name; //returns the facility name
     }
 
     public String getDescription() {
-        return description;
+        return description; //returns the description
     }
 }
