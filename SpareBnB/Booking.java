81822bc20d099ea3cefa65d881afee396821ac58
kisanbura <kisanbura48@gmail.com>
2025-11-24 06:24:18 +0000

    Update Booking.java

--- SpareBnB/Booking.java
+++ SpareBnB/Booking.java
@@ -1,36 +1,38 @@
 public class Booking {
 
-    private User user;
-    private Accommodation accommodation;
-    private int nights;
+    private User user;   //links user to booking
+    private Accommodation accommodation;   //links accom
+    private int nights;   //tracks number of nights
     private double totalCost;
-    private boolean successful;
+    private boolean successful; //success state
 
     public Booking(User user, Accommodation accommodation, int nights) {
 
-        this.user = user;
+        this.user = user;    //creates a booking attempt
         this.accommodation = accommodation;
         this.nights = nights;
 
         // Attempt booking
-        successful = accommodation.book(user);
+        successful = accommodation.book(user); //calls accommodation.book()
+        // if bookedBy is null - success
+        // if already booked - returns false
 
         if (successful) {
-            totalCost = accommodation.getTotalPrice(nights);
+            totalCost = accommodation.getTotalPrice(nights);   // if successful calculates full cost
             System.out.println("Booking successful for " + user.getFullName() +
-                    " at " + accommodation.getName());
+                    " at " + accommodation.getName()); // confirmation message
         } else {
             totalCost = 0;
             System.out.println("Cannot create booking for " + user.getFullName() +
                     ". Accommodation " + accommodation.getName() +
                     " is already booked by " + accommodation.getBookedBy().getFullName() +
-                    " and unavailable.");
+                    " and unavailable.");      // shows who already booked it if not successful
         }
     }
 
     public boolean isSuccessful() { return successful; }
 
-    public void releaseBooking() {
+    public void releaseBooking() {  //release booking
         if (successful) {
             accommodation.releaseBooking();
             successful = false;
