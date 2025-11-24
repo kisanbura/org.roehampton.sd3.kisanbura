5a296da9eea132acd9364c2ba2b33104d92bf597
kisanbura <kisanbura48@gmail.com>
2025-11-24 06:25:11 +0000

    Update Hotel.java

--- SpareBnB/Hotel.java
+++ SpareBnB/Hotel.java
@@ -2,16 +2,16 @@ import java.util.List;
 
 public class Hotel extends Accommodation {
 
-    private int starRating;
+    private int starRating; //property unique to hotel
 
-    public Hotel(String name, String location, double pricePerNight, int capacity,
+    public Hotel(String name, String location, double pricePerNight, int capacity,   //constructor for hotel
                  List<Facility> facilities, int starRating) {
-        super(name, location, pricePerNight, capacity, facilities);
-        this.starRating = starRating;
+        super(name, location, pricePerNight, capacity, facilities); //calling parent class constructor
+        this.starRating = starRating;   //saves hotel specific info
     }
 
     @Override
-    public String getDescription() {
+    public String getDescription() {    // polymorphism - each subclass has own description
         return starRating + "★ luxury hotel";
     }
 }
