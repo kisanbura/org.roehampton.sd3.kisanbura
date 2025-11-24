37ee9f393ccd49123093740ec1269854d43c7398
kisanbura <kisanbura48@gmail.com>
2025-11-24 03:14:11 +0000

    Update Hotel.java

--- SpareBnB/Hotel.java
+++ SpareBnB/Hotel.java
@@ -1,23 +1,17 @@
-public class Hotel extends Accommodation {
-    private int starRating;
-    private boolean hasRestaurant;
-    private boolean hasGym;
-    private boolean hasPool;
+import java.util.List;
 
-    public Hotel(String name, String location, double pricePerNight, int capacity, Facility facility,
-                 int starRating, boolean hasRestaurant, boolean hasGym, boolean hasPool) {
-        super(name, location, pricePerNight, capacity, facility);
+public class Hotel extends Accommodation {
+
+    private int starRating;
+
+    public Hotel(String name, String location, double pricePerNight, int capacity,
+                 List<Facility> facilities, int starRating) {
+        super(name, location, pricePerNight, capacity, facilities);
         this.starRating = starRating;
-        this.hasRestaurant = hasRestaurant;
-        this.hasGym = hasGym;
-        this.hasPool = hasPool;
     }
 
     @Override
     public String getDescription() {
-        return starRating + "★ Hotel with" +
-                (hasRestaurant ? " restaurant," : "") +
-                (hasGym ? " gym," : "") +
-                (hasPool ? " pool." : "");
+        return starRating + "★ luxury hotel";
     }
 }
