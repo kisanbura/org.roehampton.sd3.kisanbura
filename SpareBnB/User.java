6a75bba171845cbdefaf1ae5cf8ddb0aa9ec80ae
kisanbura <kisanbura48@gmail.com>
2025-11-24 03:15:01 +0000

    Update User.java

--- SpareBnB/User.java
+++ SpareBnB/User.java
@@ -1,32 +1,53 @@
 public class User {
-    private String firstName;
-    private String secondName;
+    private String fullName;
     private int phoneNumber;
     private String dob;
+    private String email;
 
-    public User(String firstName, String secondName, int phoneNumber, String dob) {
-        this.firstName = firstName;
-        this.secondName = secondName;
+    public User(String fullName, int phoneNumber, String dob, String email) {
+        this.fullName = fullName;
         this.phoneNumber = phoneNumber;
+        this.dob = dob;
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
         this.dob = dob;
     }
 
-    public String getFirstName() { return firstName; }
-    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public String getEmail() {
+        return email;
+    }
 
-    public String getSecondName() { return secondName; }
-    public void setSecondName(String secondName) { this.secondName = secondName; }
-
-    public int getPhoneNumber() { return phoneNumber; }
-    public void setPhoneNumber(int phoneNumber) { this.phoneNumber = phoneNumber; }
-
-    public String getDob() { return dob; }
-    public void setDob(String dob) { this.dob = dob; }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public void printUser() {
-        System.out.println("User: " + firstName + " " + secondName);
+        System.out.println("User: " + fullName);
         System.out.println("Phone: " + phoneNumber);
         System.out.println("DOB: " + dob);
+        System.out.println("Email: " + email);
         System.out.println("-----------------------------------");
     }
 }
